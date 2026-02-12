44a53cfffc7a851a4040c1a8a248ec63d0ea9504
Nayan Islam <iamnayanislam@gmail.com>
2026-02-12 08:47:37 +0600

    TS Config

---
+++ config/dist/index.js
@@ -0,0 +1,3 @@
+console.log("I'm a TypeScript file converted into JavaScript!!!");
+export {};
+//# sourceMappingURL=index.js.map
---
+++ config/src/index.ts
@@ -0,0 +1,1 @@
+console.log("I'm a TypeScript file converted into JavaScript!!!");
